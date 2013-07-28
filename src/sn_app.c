bec99cd8c48e9cde3ea2be7576fbf5ba5212d491
Rafal Mielniczuk <rafal.mielniczuk2@gmail.com>
2013-07-28 21:05:50 +0200

    udev: use udev to find primary gpu

--- src/sn_app.c
+++ src/sn_app.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include <signal.h>
+#include <libudev.h>
+#include <string.h>
 
 #include "sn_app.h"
 
@@ -16,6 +18,8 @@ struct sn_app {
 
 	struct ev_event_source *signal_source;
 	struct app_video *video;
+
+	struct udev *udev;
 };
 
 static int
@@ -27,6 +31,51 @@ sn_app_signal_handler(int signal, void *data)
 		ev_event_loop_stop(app->event_loop);
 		ev_event_source_remove(app->signal_source);
 	}
+}
+
+static struct udev_device *
+find_primary_gpu(struct sn_app *app)
+{
+	struct udev_enumerate *e;
+	struct udev_device *device, *drm_device, *pci;
+	struct udev_list_entry *entry;
+	const char *path, *bvga;
+
+	e = udev_enumerate_new(app->udev);
+	udev_enumerate_add_match_subsystem(e, "drm");
+	udev_enumerate_add_match_sysname(e, "card[0-9]*");
+
+	udev_enumerate_scan_devices(e);
+
+	drm_device = NULL;
+	udev_list_entry_foreach(entry, udev_enumerate_get_list_entry(e)) {
+		path = udev_list_entry_get_name(entry);
+		device = udev_device_new_from_syspath(app->udev, path);
+
+		pci = udev_device_get_parent_with_subsystem_devtype(device,
+								    "pci", NULL);
+
+		if (pci) {
+			bvga = udev_device_get_sysattr_value(pci, "boot_vga");
+
+			if (bvga != NULL && !strcmp(bvga, "1")) {
+				if (drm_device) {
+					udev_device_unref(drm_device);
+				}
+				drm_device = device;
+				break;
+			}
+		}
+
+		if (!drm_device) {
+			drm_device = device;
+		} else {
+			udev_device_unref(device);
+		}
+	}
+
+	udev_enumerate_unref(e);
+	return drm_device;
 }
 
 CM_EXPORT void
@@ -53,6 +102,8 @@ CM_EXPORT struct sn_app *
 sn_app_create(void)
 {
 	struct sn_app *app;
+	struct udev_device *drm_dev;
+	const char *node;
 
 	app = malloc(sizeof *app);
 	if (!app)
@@ -61,7 +112,25 @@ sn_app_create(void)
 	app->ref = 1;
 	app->event_loop = ev_event_loop_create();
 
-	app->video = app_video_create(app->event_loop, "/dev/dri/card0");
+	app->udev = udev_new();
+
+	if (!app->udev) {
+		log_fatal("failed to create udev")
+		goto err_udev;
+	}
+
+	drm_dev = find_primary_gpu(app);
+
+	if (!drm_dev) {
+		log_fatal("failed to find primary gpu")
+		goto err_udev;
+	}
+
+	node = udev_device_get_devnode(drm_dev);
+
+	log_info("found primary gpu: %s", node)
+
+	app->video = app_video_create(app->event_loop, node);
 
 	if (!app->video) {
 		log_fatal("failed to init the video");
@@ -82,9 +151,12 @@ sn_app_create(void)
 
 	return app;
 
+
 err_evloop:
 	app_video_unref(app->video);
 err_video:
+	udev_unref(app->udev);
+err_udev:
 	ev_event_loop_unref(app->event_loop);
 	free(app);
 	return NULL;
--- src/snake.c
+++ src/snake.c
@@ -9,19 +9,20 @@
 
 int main(int argc, char *argv[])
 {
-    cm_log_configure(argc, argv);
+	cm_log_configure(argc, argv);
 
 	struct sn_app *app = sn_app_create();
 
 	if (!app) {
-		log_fatal("failed to create the app\n");
+		log_fatal("failed to create the app");
 		return 1;
 	}
 
 	log_info("running the app")
+
 	sn_app_run(app);
 
-	log_info("finishing the app");
+	log_info("finishing the app")
 
 	sn_app_unref(app);
 	return 0;
